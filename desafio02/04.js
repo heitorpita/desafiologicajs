87d0b1e6ff41e02e54efcc0afbd4dfb8767bfc72
Heitor <heitor@tom.tom>
2025-04-27 17:01:07 -0300

    second commit

---
+++ desafio02/04.js
@@ -0,0 +1,13 @@
+// Escreva uma função chamada ePaisagem que 
+// recebe dois argumentos, largura e altura
+// de uma imagem (number).
+// retorne true se a imagem estiver no modo paisagem
+
+
+function ePaisagem(largura, altura) {
+    return largura > altura ? true : false;
+
+}
+
+console.log(ePaisagem(1920, 1080));
+
